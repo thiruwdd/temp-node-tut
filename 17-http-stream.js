63a080067ac3cafb97f617adf57b959a7c5dd5b6
Thirumalesh <thiruwdd@gmail.com>
2023-12-23 14:51:36 +0530

    17-http-stream

--- 17-http-stream.js
+++ 17-http-stream.js
@@ -2,6 +2,11 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer((req,res) => {
-    const text = fs.readFileSync('./content/big.txt', 'utf8')
-    res.end(text)
-}).listen(5000);
+    var fileStrem = fs.createReadStream('./content/big.txt', 'utf8');
+    fileStrem.on('open', () => {
+        fileStrem.pipe(res)
+    })
+    fileStrem.on('error', (err) => {
+        res.end(err)
+    })
+}).listen(5000)
